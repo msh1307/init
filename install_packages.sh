d4ba70441c1c727faa5a1a22930a32fe8ebd1396
msh1307 <bioloidgp@gmail.com>
2023-08-21 20:01:05 +0900

    dfasdfasdfasdf

---
+++ install_packages.sh
@@ -0,0 +1,34 @@
+cd ~/
+sudo apt update
+sudo apt install qemu-user-static file
+sudo apt install vim git gcc ssh curl wget gdb sudo zsh python3 python3-pip libffi-dev build-essential libssl-dev libc6-i386 libc6-dbg gcc-multilib make -y
+sudo apt install -y qemu-system qemu-kvm
+sudo apt install python-is-python3
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install libc6:i386 -y
+python3 -m pip install --upgrade pip
+pip3 install unicorn
+pip3 install keystone-engine
+pip3 install pwntools
+pip3 install ropgadget
+sudo apt install libcapstone-dev -y
+git clone https://github.com/pwndbg/pwndbg
+cd pwndbg
+./setup.sh
+cd ..
+echo source ~/pwndbg/gdbinit.py >> ~/.gdbinit
+sudo apt install ruby-full -y
+sudo gem install one_gadget seccomp-tools
+sudo wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+mkdir -p "$HOME/.zsh"
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+echo "fpath+=("$HOME/.zsh/pure")\nautoload -U promptinit; promptinit\nprompt pure" >> ~/.zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ~/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+echo "ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=111'" >> ~/.zshrc
+git clone https://github.com/msh1307/init
+cd init
+./go.sh
