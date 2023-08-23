322dc72af9dce00d829b8941775234b79f6b1bfe
root <root@WIN-ICUK6SGIFLG>
2023-08-24 03:06:04 +0900

    asdf

--- install_packages.sh
+++ install_packages.sh
@@ -19,7 +19,15 @@ git clone https://github.com/NixOS/patchelf
 cd pwndbg
 ./setup.sh
 cd ..
-echo "source ~/pwndbg/gdbinit.py\nsource ~/Pwngdb/pwngdb.py\nsource ~/Pwngdb/angelheap/gdbinit.py\ndefine hook-run\npython\nimport angelheap\nangelheap.init_angelheap()\nend\nend" >> ~/.gdbinit
+echo "source ~/pwndbg/gdbinit.py" >> ~/.gdbinit
+echo "source ~/Pwngdb/pwngdb.py" >> ~/.gdbinit
+echo "source ~/Pwngdb/angelheap/gdbinit.py" >> ~/.gdbinit
+echo "define hook-run" >> ~/.gdbinit
+echo "python" >> ~/.gdbinit
+echo "import angelheap" >> ~/.gdbinit
+echo "angelheap.init_angelheap()" >> ~/.gdbinit
+echo "end" >> ~/.gdbinit
+echo "end" >> ~/.gdbinit
 sudo apt install ruby-full -y
 sudo gem install one_gadget seccomp-tools
 sudo wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
